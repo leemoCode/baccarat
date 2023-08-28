d93d41b91e12a52092ede510e356d255e8a70b08
leemo <L777888@88.com>
2023-08-28 14:41:50 +0800

    feat: 初始化项目

---
+++ src/render/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    百家乐
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+
+// import Demo from './components/Demo.vue';
+
+export default defineComponent({
+  name: 'Template',
+  components: {
+    // Demo,
+  },
+  setup() {
+    onMounted(() => {
+    });
+    return {
+    };
+  },
+});
+</script>
+<style lang="scss" scoped></style>
